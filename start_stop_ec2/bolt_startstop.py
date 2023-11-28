3a1133e542629045d09138756bb21d507688a608
Sorin <52976564+sorman04@users.noreply.github.com>
2023-11-28 07:41:33 +0200

    added secrets to startstop

--- start_stop_ec2/bolt_startstop.py
+++ start_stop_ec2/bolt_startstop.py
@@ -19,8 +19,19 @@
 import json
 import boto3
 
-region = "eu-central-1"
+# get AWS Secrets Manager
+secret_name = "AWS_AccessKeys"
+region_name = "eu-north-1"
+session = boto3.session.Session()
+client = session.client(service_name="secretsmanager", region_name=region_name)
+# call secrets
+get_secrets = client.get_secret_value(SecretId=secret_name)
+secrets = get_secrets["SecretString"]
+secrets_json = json.loads(secrets)
+AWS_ACCESS_KEY_ID = secrets_json["Access_key_ID"]
+AWS_SECRET_ACCESS_KEY = secrets_json["Secret_Access_key"]
 
+region = "eu-central-1"
 ec2 = boto3.client("ec2", region_name=region)
 
 
